406c466f885c5eb4f4a155245d0ca8499d85aae6
Vadims Podans <bokorezi@inbox.lv>
2018-02-03 15:10:24 +0200

    Version upgrade: 3.2.6 -> 3.2.7

--- PKI/Properties/AssemblyInfo.cs
+++ PKI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sysadmins LV")]
 [assembly: AssemblyProduct("PowerShell PKI Module")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2014, Vadims Podans")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2018, Vadims Podans")]
 [assembly: AssemblyTrademark("Sysadmins LV")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.6.0")]
-[assembly: AssemblyFileVersion("3.2.6.0")]
+[assembly: AssemblyVersion("3.2.7.0")]
+[assembly: AssemblyFileVersion("3.2.7.0")]
