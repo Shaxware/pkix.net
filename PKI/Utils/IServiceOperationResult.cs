abab0dc8f564348c4d1f7d13d5bd0e4ae3b59ee3
Vadims Podans <bokorezi@inbox.lv>
2018-02-14 20:13:08 +0200

    added result objects for PSPKI

---
+++ PKI/Utils/IServiceOperationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PKI.Utils {
+	/// <summary>
+	/// Defines general-purpose interface to indicate the status of the operation.
+	/// </summary>
+	public interface IServiceOperationResult {
+		/// <summary>
+		/// Gets the Win32-style HRESULT error code. Zero value means success, non-zero value means failure.
+		/// </summary>
+		Int32 HResult { get; }
+		/// <summary>
+		/// Gets the message associated with the <see cref="HResult"/> error code.
+		/// </summary>
+		String StatusMessage { get; }
+		/// <summary>
+		/// Gets or sets the object which is the result of the operation. This member is not mandatory and may
+		/// return <strong>null</strong> even if the status is successful and depends.
+		/// </summary>
+		Object InnerObject { get; set; }
+	}
+}
---
+++ PKI/Utils/ServiceOperationResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PKI.Utils {
+	/// <summary>
+	/// Defines general-purpose class to indicate the status of the operation.
+	/// </summary>
+	public class ServiceOperationResult : IServiceOperationResult {
+
+		/// <summary>
+		/// Initializes a new instance of <strong>ServiceOperationResult</strong> from Win32 HRESULT status code.
+		/// Status message is decoded automatically.
+		/// </summary>
+		/// <param name="hresult">32-bit Win32 HRESULT status code.</param>
+		public ServiceOperationResult(Int32 hresult) {
+			HResult = hresult;
+			StatusMessage = Error.GetMessage(hresult);
+		}
+		/// <summary>
+		/// Initializes a new instance of <strong>ServiceOperationResult</strong> from Win32 HRESULT status code
+		/// and custome status message.
+		/// </summary>
+		/// <param name="hresult">32-bit Win32 HRESULT status code.</param>
+		/// <param name="message">Custom status message for specified status code.</param>
+		public ServiceOperationResult(Int32 hresult, String message) {
+			HResult = hresult;
+			StatusMessage = message;
+		}
+		/// <summary>
+		/// Initializes a new instance of <strong>ServiceOperationResult</strong> from Win32 HRESULT status code
+		/// and custome status message.
+		/// </summary>
+		/// <param name="hresult">32-bit Win32 HRESULT status code.</param>
+		/// <param name="message">Custom status message for specified status code.</param>
+		/// <param name="obj">Custom object associated with the current operation status.</param>
+		public ServiceOperationResult(Int32 hresult, String message, Object obj) {
+			HResult = hresult;
+			StatusMessage = message;
+			InnerObject = obj;
+		}
+
+		/// <inheritdoc />
+		public Int32 HResult { get; }
+		/// <inheritdoc />
+		public String StatusMessage { get; }
+		/// <inheritdoc />
+		public Object InnerObject { get; set; }
+	}
+}
