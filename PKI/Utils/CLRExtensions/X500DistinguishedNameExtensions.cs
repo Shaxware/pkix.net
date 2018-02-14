4c8b3eb224953c279f0acdc7782f95336a7ae6b7
Vadims Podans <bokorezi@inbox.lv>
2018-02-14 19:31:20 +0200

    Changed return type to X500RdnAttributeCollection

--- PKI/Utils/CLRExtensions/X500DistinguishedNameExtensions.cs
+++ PKI/Utils/CLRExtensions/X500DistinguishedNameExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using SysadminsLV.Asn1Parser;
@@ -14,13 +13,13 @@ namespace PKI.Utils.CLRExtensions {
 		/// </summary>
 		/// <param name="name"></param>
 		/// <returns></returns>
-		public static X500RdnAttribute[] GetRdnAttributes(this X500DistinguishedName name) {
+		public static X500RdnAttributeCollection GetRdnAttributes(this X500DistinguishedName name) {
 			if (name == null) { throw new ArgumentNullException(nameof(name)); }
 			if (name.RawData == null || name.RawData.Length == 0) { return null; }
 			Asn1Reader asn = new Asn1Reader(name.RawData);
 			if (!asn.MoveNext()) { return null; }
 			if (asn.NextCurrentLevelOffset == 0) { return null; }
-			var retValue = new List<X500RdnAttribute>();
+			var retValue = new X500RdnAttributeCollection();
 			do {
 				Asn1Reader asn2 = new Asn1Reader(asn.GetPayload());
 				asn2.MoveNext();
@@ -30,7 +29,7 @@ namespace PKI.Utils.CLRExtensions {
 				retValue.Add(new X500RdnAttribute(oid, value));
 
 			} while (asn.MoveNextCurrentLevel());
-			return retValue.ToArray();
+			return retValue;
 		}
 	}
 }
