0de3254a6233a4b6e3e3992575e78057cd346d4d
crypt32 <vpodans@sysadmins.lv>
2018-03-29 20:41:05 +0300

    Added GetCryptBlob extension method for PublicKey class to retrieve CNG-compatible public key format.

--- PKI/Utils/CLRExtensions/PublicKeyExtensions.cs
+++ PKI/Utils/CLRExtensions/PublicKeyExtensions.cs
@@ -22,7 +22,18 @@ namespace PKI.Utils.CLRExtensions {
                     readRsaHeader(blob, publicKey);
                     break;
                 // DSA
-                case "1.2.840.10040.4.1": break;
+                case "1.2.840.10040.4.1":
+                    // DSA has two formats, legacy and new. Legacy DSA keys are up to 1024 bit and support
+                    // SHA1 hash algorithm only. Larger keys support up to 2048? bit keys and new hashing algorithms,
+                    // SHA1, SHA256 and SHA512. SHA384 somehow is missing, see bcrypt.h file for
+                    // HASHALGORITHM_ENUM
+                    // so check the key size and read appropriate header
+                    if (publicKey.Key.KeySize <= 1024) {
+                        readDsaV1Header(blob, publicKey);
+                    } else {
+                        readDsaV2Header(blob, publicKey);
+                    }
+                    break;
                 // ECC/ECDSA
                 case "1.2.840.10045.2.1":
                     readEcdsaHeader(blob, publicKey);
@@ -32,7 +43,6 @@ namespace PKI.Utils.CLRExtensions {
         }
 
         static void readRsaHeader(List<Byte> blob, PublicKey publicKey) {
-            Int32 pubKeyLength = publicKey.Key.KeySize;
             /*
             typedef struct _BCRYPT_RSAKEY_BLOB {
               ULONG Magic;          -- 0x31415352
@@ -43,6 +53,7 @@ namespace PKI.Utils.CLRExtensions {
               ULONG cbPrime2;       -- const 0
             } BCRYPT_RSAKEY_BLOB; -- public key only
             */
+            Int32 pubKeyLength = publicKey.Key.KeySize;
             blob.AddRange(BitConverter.GetBytes(RSA_MAGIC));        // Magic
             blob.AddRange(BitConverter.GetBytes(pubKeyLength));     // bitLen
             blob.AddRange(BitConverter.GetBytes(3));                // cbPublicExp
@@ -50,8 +61,100 @@ namespace PKI.Utils.CLRExtensions {
             blob.AddRange(new Byte[] { 0, 0, 0, 0, 0, 0, 0, 0 });   // zero length primes
             blob.AddRange(publicKey.EncodedKeyValue.RawData);
         }
-        static void readDsaHeader(List<Byte> blob, PublicKey publicKey) {
+        static void readDsaV1Header(List<Byte> blob, PublicKey publicKey) {
+            /*
+            typedef struct _BCRYPT_DSA_KEY_BLOB {
+                ULONG dwMagic;
+                ULONG cbKey;
+                UCHAR Count[4];
+                UCHAR Seed[20];
+                UCHAR q[20];
+            } BCRYPT_DSA_KEY_BLOB, *PBCRYPT_DSA_KEY_BLOB; -- public key only
+            */
+            blob.AddRange(BitConverter.GetBytes(DSA_V1_MAGIC));
+            blob.AddRange(BitConverter.GetBytes(publicKey.Key.KeySize));
+            DSAParameters parameters = ((DSACryptoServiceProvider)publicKey.Key).ExportParameters(false);
+            if (parameters.Seed == null) {
+                // fill count and seed values with 0xff
+                for (Int32 index = 0; index < 24; index++) {
+                    blob.Add(0xff);
+                }
+            } else {
+                // seed is exactly 20 bytes long
+                if (parameters.Seed.Length != 20) {
+                    throw new ArgumentException("Seed parameter is incorrect.");
+                }
+                blob.AddRange(BitConverter.GetBytes(parameters.Counter));
+                blob.AddRange(parameters.Seed);
+            }
+            blob.AddRange(parameters.Q);
+            /*
+            BCRYPT_DSA_KEY_BLOB
+            Modulus[cbKey]    // Big-endian.
+            Generator[cbKey]  // Big-endian.
+            Public[cbKey]     // Big-endian.
+            */
+            blob.AddRange(parameters.P);
+            blob.AddRange(parameters.G);
+            blob.AddRange(parameters.Y);
+        }
+        static void readDsaV2Header(List<Byte> blob, PublicKey publicKey) {
+            /*
+            typedef struct _BCRYPT_DSA_KEY_BLOB_V2 {
+                ULONG               dwMagic;
+                ULONG               cbKey;
+                HASHALGORITHM_ENUM  hashAlgorithm;      -- SHA1 - 0, SHA256 - 1, SHA512 - 2
+                DSAFIPSVERSION_ENUM standardVersion;    -- DSA_FIPS186_2 - 0, 
+                ULONG               cbSeedLength;
+                ULONG               cbGroupSize;
+                UCHAR               Count[4];
+            } BCRYPT_DSA_KEY_BLOB_V2, *PBCRYPT_DSA_KEY_BLOB_V2;
+            according to: https://msdn.microsoft.com/en-us/library/windows/desktop/jj670561(v=vs.85).aspx
+            this struct is available only starting with Windows 8/Windows Server 2012
+            */
+            Version version = Environment.OSVersion.Version;
+            if ((version.Major != 6 || version.Minor < 2) && version.Major <= 6) {
+                throw new PlatformNotSupportedException("DSAv2 keys are supported on systems started with Windows 8/Windows Server 2012.");
+            }
+            blob.AddRange(BitConverter.GetBytes(DSA_V2_MAGIC));
+            blob.AddRange(BitConverter.GetBytes(publicKey.Key.KeySize));
+            DSAParameters parameters = ((DSACryptoServiceProvider)publicKey.Key).ExportParameters(false);
+            switch (parameters.Q.Length) {
+                case 20:
+                    blob.AddRange(BitConverter.GetBytes((Int32)BCRYPT_HASHALGORITHM_ENUM.DSA_HASH_ALGORITHM_SHA1));
+                    break;
+                case 32:
+                    blob.AddRange(BitConverter.GetBytes((Int32)BCRYPT_HASHALGORITHM_ENUM.DSA_HASH_ALGORITHM_SHA256));
+                    break;
+                case 64:
+                    blob.AddRange(BitConverter.GetBytes((Int32)BCRYPT_HASHALGORITHM_ENUM.DSA_HASH_ALGORITHM_SHA512));
+                    break;
+                default:
+                    throw new ArgumentException("Q-parameter is invalid");
+            }
+            blob.AddRange(BitConverter.GetBytes((Int32)BCRYPT_DSAFIPSVERSION_ENUM.DSA_FIPS186_3));
+            if (parameters.Seed == null) {
+                // fill cbSeedLength with 0xff
+                for (Int32 index = 0; index < parameters.Q.Length + 4; index++) {
+                    blob.Add(0xff);
+                }
+            } else {
+                blob.AddRange(BitConverter.GetBytes(parameters.Seed.Length));
+                blob.AddRange(BitConverter.GetBytes(parameters.Q.Length));
+                blob.AddRange(BitConverter.GetBytes(parameters.Counter));
+                blob.AddRange(parameters.Seed);
+            }
 
+            blob.AddRange(parameters.Q);
+            /*
+            BCRYPT_DSA_KEY_BLOB
+            Modulus[cbKey]    // Big-endian.
+            Generator[cbKey]  // Big-endian.
+            Public[cbKey]     // Big-endian.
+            */
+            blob.AddRange(parameters.P);
+            blob.AddRange(parameters.G);
+            blob.AddRange(parameters.Y);
         }
         static void readEcdsaHeader(List<Byte> blob, PublicKey publicKey) {
             /*
@@ -82,6 +185,17 @@ namespace PKI.Utils.CLRExtensions {
             }
             // skip first byte, it is always 0X04 for ECDSA public key
             blob.AddRange(publicKey.EncodedKeyValue.RawData.Skip(1));
+        }
+
+        enum BCRYPT_HASHALGORITHM_ENUM {
+            DSA_HASH_ALGORITHM_SHA1 = 0,
+            DSA_HASH_ALGORITHM_SHA256 = 1,
+            DSA_HASH_ALGORITHM_SHA512 = 2
+        }
+
+        enum BCRYPT_DSAFIPSVERSION_ENUM {
+            DSA_FIPS186_2 = 0,
+            DSA_FIPS186_3 = 1
         }
     }
 }
