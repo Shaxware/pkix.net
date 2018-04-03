a34516aa5dd7ca19455bd7c6e1c0e56580187af5
crypt32 <vpodans@sysadmins.lv>
2018-04-03 19:53:07 +0300

    Added AdcsDbPropertyCollection class and reference in RequestRow class.

--- PKI/CertificateServices/DB/RequestRow.cs
+++ PKI/CertificateServices/DB/RequestRow.cs
@@ -1,35 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace PKI.CertificateServices.DB {
-	/// <summary>
-	/// Represents Certification Authority database row. This object contains only 4 base properties:
-	/// <see cref="RowId"/>, <see cref="RequestId"/>, <see cref="ConfigString"/> and <see cref="Table"/>.
-	/// Other properties should be added by using external means (for example, by using Add-Member cmdlet in
-	///	Windows PowerShell).
-	/// </summary>
-	public class RequestRow {
-		/// <summary>
-		/// Gets or sets RowId which corresponds to row number in CA database.
-		/// </summary>
-		public Int32 RowId { get; set; }
-		/// <summary>
-		/// Gets or sets RequestId which corresponds to request ID number in CA database. This property is set to zero
-		/// for non-request tables.
-		/// </summary>
-		public Int32 RequestId { get; set; }
-		/// <summary>
-		/// Gets or sets the configuration string of the CA server to which this object is related.
-		/// </summary>
-		public String ConfigString { get; set; }
-		/// <summary>
-		/// Gets or sets database table name.
-		/// </summary>
-		public TableList Table { get; set; }
-		/// <summary>
-		/// Gets a collection of properties associated with the current row object.
-		/// </summary>
-		IDictionary<String, AdcsDbValue> Properties { get; } = new Dictionary<String, AdcsDbValue>();
-		
-	}
+    /// <summary>
+    /// Represents Certification Authority database row. This object contains only 4 base properties:
+    /// <see cref="RowId"/>, <see cref="RequestId"/>, <see cref="ConfigString"/> and <see cref="Table"/>.
+    /// Other properties should be added by using external means (for example, by using Add-Member cmdlet in
+    ///	Windows PowerShell).
+    /// </summary>
+    public class RequestRow {
+        /// <summary>
+        /// Gets or sets RowId which corresponds to row number in CA database.
+        /// </summary>
+        public Int32 RowId { get; set; }
+        /// <summary>
+        /// Gets or sets RequestId which corresponds to request ID number in CA database. This property is set to zero
+        /// for non-request tables.
+        /// </summary>
+        public Int32 RequestId { get; set; }
+        /// <summary>
+        /// Gets or sets the configuration string of the CA server to which this object is related.
+        /// </summary>
+        public String ConfigString { get; set; }
+        /// <summary>
+        /// Gets or sets database table name.
+        /// </summary>
+        public TableList Table { get; set; }
+        /// <summary>
+        /// Gets a collection of properties associated with the current row object.
+        /// </summary>
+        public AdcsDbPropertyCollection Properties { get; } = new AdcsDbPropertyCollection();
+        
+    }
 }
