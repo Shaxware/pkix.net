50a92f0573fdc9fe57cc1cc96fc0d10c9e234634
crypt32 <vpodans@sysadmins.lv>
2018-06-27 21:12:02 +0300

    Added CAIssuedCertValidityConfig class

---
+++ PKI/Management/CertificateServices/Configuration/AdcsValidityUnit.cs
@@ -0,0 +1,31 @@
+﻿namespace SysadminsLV.PKI.Management.CertificateServices.Configuration {
+    /// <summary>
+    /// Contains enumeration of common validity units used by CA server.
+    /// </summary>
+    public enum AdcsValidityUnit {
+        /// <summary>
+        /// Specifies invalid period unit type.
+        /// </summary>
+        Invalid,
+        /// <summary>
+        /// Period is measured in hours.
+        /// </summary>
+        Hours,
+        /// <summary>
+        /// Period is measured in Days.
+        /// </summary>
+        Days,
+        /// <summary>
+        /// Period is measured in Weeks.
+        /// </summary>
+        Weeks,
+        /// <summary>
+        /// Period is measured in Months.
+        /// </summary>
+        Months,
+        /// <summary>
+        /// Period is measured in years.
+        /// </summary>
+        Years
+    }
+}
---
+++ PKI/Management/CertificateServices/Configuration/CAIssuedCertValidityConfig.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using PKI.CertificateServices;
+
+namespace SysadminsLV.PKI.Management.CertificateServices.Configuration {
+    /// <summary>
+    /// Represents Certification Authority object with defined maximum validity period of issued certificates.
+    /// </summary>
+    /// <remarks>These settings are not ultimate. Issued certificate's effective validity period is the smallest
+    /// value of:
+    /// <list type="bullet">
+    /// <item>Remaining validity of the CA certificate.</item>
+    /// <item>ValidityPeriod registry settings (this object implements ValidityPeriod setting).</item>
+    /// <item>Validity defined in certificate template (Enterprise CAs only).</item>
+    /// <item>Validity period specified in certificate request.</item>
+    /// </list> 
+    /// </remarks>
+    public sealed class CAIssuedCertValidityConfig : AdcsConfigurationEntry {
+        readonly AdcsInternalConfigPath _period, _units;
+
+        /// <inheritdoc />
+        public CAIssuedCertValidityConfig(CertificateAuthority certificateAuthority) : base(certificateAuthority) {
+            _period = new AdcsInternalConfigPath { ValueName = "ValidityPeriodUnits" };
+            _units = new AdcsInternalConfigPath { ValueName = "ValidityPeriod" };
+            RegEntries.Add(_period);
+            RegEntries.Add(_units);
+            ReadConfig();
+        }
+
+        /// <summary>
+        /// Gets or sets validity period of issued certificates. Period value is measured in units specified in
+        /// <see cref="ValidityPeriodUnits"/> member.
+        /// </summary>
+        public Int32 ValidityPeriod {
+            get => (Int32)_period.Value;
+            set {
+                if ((Int32)_period.Value != value) {
+                    _period.Value = value;
+                    IsModified = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets validity period unit measure.
+        /// </summary>
+        public AdcsValidityUnit ValidityPeriodUnits {
+            get {
+                Enum.TryParse(_units.Value.ToString(), true, out AdcsValidityUnit unit);
+                return unit;
+            }
+            set {
+                if (value != AdcsValidityUnit.Invalid) {
+                    _units.Value = value.ToString();
+                    IsModified = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Gets a read-only property that displays a composed value of this setting.
+        /// </summary>
+        public String ValidityPeriodString => $"{ValidityPeriod} {ValidityPeriodUnits}";
+    }
+}
