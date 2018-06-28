4c906802e3a0442a44d012b4f56d9d12af34ccde
crypt32 <vpodans@sysadmins.lv>
2018-06-28 22:58:22 +0300

    Added CaCertViewTimoutConfig class

---
+++ PKI/Management/CertificateServices/Configuration/CaCertViewTimoutConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using PKI.CertificateServices;
+using SysadminsLV.PKI.Management.CertificateServices.Configuration;
+
+namespace PKI.Management.CertificateServices.Configuration {
+    /// <summary>
+    /// Represents ADCS database timeout settings. There are two types of timeouts: idle timeout and
+    /// connection timeout. When idle timeout is reached, a new connection to CA database must be created.
+    /// When connection timeout is reached, a new connection to CA database must be created.
+    /// </summary>
+    public sealed class CaCertViewTimoutConfig : AdcsConfigurationEntry {
+        readonly AdcsInternalConfigPath _idleTimeout, _ageTimeout;
+
+        /// <inheritdoc />
+        public CaCertViewTimoutConfig(CertificateAuthority certificateAuthority) : base(certificateAuthority) {
+            _idleTimeout = new AdcsInternalConfigPath { ValueName = "ViewIdleMinutes" };
+            _ageTimeout = new AdcsInternalConfigPath { ValueName = "ViewAgeMinutes" };
+            ReadConfig();
+        }
+
+        /// <summary>
+        /// Gets or sets CA database idle connection timeout in minutes. Default is 8 minutes. If there are no
+        /// activity within active connection for specified period of time, the connection is closed by CA server
+        /// and new connection to CA database must be created.
+        /// </summary>
+        public Int32 IdleTimeout {
+            get => (Int32)_idleTimeout.Value;
+            set {
+                if ((Int32)_idleTimeout.Value != value) {
+                    _idleTimeout.Value = value;
+                    IsModified = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets CA database connection validity in minutes. Default is 16 minutes. When connection to CA
+        /// database is created, it is valid for specified period of time and then is closed by CA server and new
+        /// connection to CA database must be created.
+        /// </summary>
+        public Int32 ViewTimeout {
+            get => (Int32)_ageTimeout.Value;
+            set {
+                if ((Int32)_ageTimeout.Value != value) {
+                    _ageTimeout.Value = value;
+                    IsModified = true;
+                }
+            }
+        }
+    }
+}
