fce4abce5e87ac84444cdadcc661df13aa9bf0b6
crypt32 <vpodans@sysadmins.lv>
2018-12-17 23:36:32 +0200

    Added ASN module

--- SysadminsLV.PKI.Cryptography/Cryptography/DSAPrivateKey.cs
+++ SysadminsLV.PKI.Cryptography/Cryptography/DSAPrivateKey.cs
@@ -1,4 +1,26 @@
 ﻿namespace SysadminsLV.PKI.Cryptography {
-    public sealed class DSAPrivateKey {
+    public sealed class DsaPrivateKey {
     }
 }
+/*
+PrivateKeyInfo ::= SEQUENCE {
+  version Version,
+  algorithm AlgorithmIdentifier,
+  PrivateKey OCTETSTRING
+}
+
+AlgorithmIdentifier ::= SEQUENCE {
+  algorithm ALGORITHM.id,
+  parameters Dss-Parms
+}
+
+Dss-Parms ::= SEQUENCE {
+  p INTEGER,
+  q INTEGER,
+  g INTEGER
+}
+
+DSAPrivateKey ::= OCTETSTRING {
+  privateExponent INTEGER
+}
+ */
