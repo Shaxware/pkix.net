5e3b6e30ef7183b1996116a62da94c7b38d25ebd
Vadims Podans <bokorezi@inbox.lv>
2019-10-24 12:35:36 +0300

    added TSP client implementation.

---
+++ PKI/Cryptography/TspResponseStatus.cs
@@ -0,0 +1,31 @@
+﻿namespace SysadminsLV.PKI.Cryptography {
+    /// <summary>
+    /// Represents possible values that identify the status of Time-Stamp request on Time-Stamp Authority (TSA)
+    /// </summary>
+    public enum TspResponseStatus {
+        /// <summary>
+        /// Response has valid confirmations and response contains requested token.
+        /// </summary>
+        Granted = 0,
+        /// <summary>
+        /// Response has valid confirmations, but response contains requested token with modifications.
+        /// </summary>
+        GrantedWithModifications,
+        /// <summary>
+        /// Request was rejected and no token is included in response.
+        /// </summary>
+        Rejected,
+        /// <summary>
+        /// Server is busy and no token is included in response.
+        /// </summary>
+        Waiting,
+        /// <summary>
+        /// Revocation is imminent and no token is included in response.
+        /// </summary>
+        RevocationWarning,
+        /// <summary>
+        /// Revoked and no token is included in response.
+        /// </summary>
+        RevocationNotification
+    }
+}
---
+++ PKI/Cryptography/TspToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SysadminsLV.PKI.Cryptography {
+    public class TspToken {
+        readonly IList<X509Extension> _extensions = new List<X509Extension>();
+
+        public Int32 Version { get; } = 1;
+        public Oid PolicyId { get; }
+        public TspMessageImprint TspMessage { get; }
+        public BigInteger SerialNumber { get; }
+        public Boolean Ordering { get; }
+        public Byte[] Nonce { get; }
+        public X509AlternativeName TsaID { get; }
+        public X509ExtensionCollection Extensions {
+            get {
+                var retValue = new X509ExtensionCollection();
+                foreach (X509Extension extension in _extensions) {
+                    retValue.Add(extension);
+                }
+                return retValue;
+            }
+        }
+    }
+}
