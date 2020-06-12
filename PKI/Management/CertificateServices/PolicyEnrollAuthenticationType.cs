0f2f3a7f070f24e7cb588edfa608cc8fa67bbd7d
Vadims Podans <vpodans@sysadmins.lv>
2020-06-12 19:49:47 +0300

    Added PolicyEnrollEndpointUri. Need to wire and replace that CESUri

---
+++ PKI/Management/CertificateServices/PolicyEnrollAuthenticationType.cs
@@ -0,0 +1,24 @@
+﻿namespace SysadminsLV.PKI.Management.CertificateServices {
+    public enum PolicyEnrollAuthenticationType {
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        None				= 0,
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        Anonymous			= 1,
+        /// <summary>
+        /// Authentication is performed using Kerberos.
+        /// </summary>
+        Kerberos			= 2,
+        /// <summary>
+        /// Authentication is performed using user name and password combination.
+        /// </summary>
+        UserNameAndPassword = 4,
+        /// <summary>
+        /// Authentication is performed using client certificate.
+        /// </summary>
+        ClientCertificate = 8
+    }
+}
---
+++ PKI/Management/CertificateServices/PolicyEnrollEndpointUri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SysadminsLV.PKI.Dcom;
+
+namespace SysadminsLV.PKI.Management.CertificateServices {
+    public class PolicyEnrollEndpointUri {
+        internal PolicyEnrollEndpointUri(ICertConfigEnrollEndpointD dcomUri) {
+            Uri = new Uri(dcomUri.Uri);
+            Authentication = (PolicyEnrollAuthenticationType)dcomUri.Authentication;
+            Priority = dcomUri.Priority;
+            RenewalOnly = dcomUri.RenewalOnly;
+        }
+
+        /// <summary>
+        /// Gets enrollment web services endpoint URL.
+        /// </summary>
+        public Uri Uri { get; }
+        /// <summary>
+        /// Gets the authentication type.
+        /// </summary>
+        public PolicyEnrollAuthenticationType Authentication { get; }
+        /// <summary>
+        /// Gets the priority of this endpoint.
+        /// </summary>
+        public Int32 Priority { get; }
+        /// <summary>
+        /// Indicates whether the endpoint is for renewal requests only (<strong>True</strong>), or accepts initial requests (<strong>False</strong>).
+        /// </summary>
+        public Boolean RenewalOnly { get; }
+    }
+}
