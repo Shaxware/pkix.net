15516b59297ebe34306cdd9ea79926c4cc140cad
Vadims Podans <vpodans@sysadmins.lv>
2020-06-12 12:46:08 +0300

    sorted parameters for ICertRegManagerD interface

---
+++ PKI/Dcom/ICertRegManagerD.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace SysadminsLV.PKI.Dcom {
+    /// <summary>
+    /// Represents a wrapper interface for Microsoft
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/certadm/nn-certadm-icertadmin2">ICertAdmin2</see> COM interface.
+    /// </summary>
+    public interface ICertRegManagerD {
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        Object GetConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry value.
+        /// </summary>
+        /// <typeparam name="T">Expected registry value type. Can be String, String[], Int32 or Byte[].</typeparam>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns></returns>
+        T GetConfigEntry<T>(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry string value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        String GetStringConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry multi-string value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        String[] GetMultiStringConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry numerical (integer) value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        Int32 GetNumericConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry boolean value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        Boolean GetBooleanConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Gets the Certification Authority configuration registry entry binary value.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to retrieve.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="FileNotFoundException">Requested entry name not found.</exception>
+        /// <returns>Requested entry value. If configuration entry does not exist, the method returns null.</returns>
+        Byte[] GetBinaryConfigEntry(String entryName, String node);
+        /// <summary>
+        /// Writes value to Certification Authority configuration. If value does not exist it will be created. Created registry value type
+        /// is inferred from <strong>data</strong> parameter.
+        /// </summary>
+        /// <param name="data">Configuration value name to write.</param>
+        /// <param name="entryName">Configuration value name to write to.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <strong>data</strong> parameter is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///     Data type for <strong>data</strong> parameter is not accepted. See remarks section for valid types.
+        /// </exception>
+        /// <remarks>
+        /// Valid types for <strong>data</strong> parameter are:
+        /// <list type="bullet">
+        ///     <item>String</item>
+        ///     <item>String[]</item>
+        ///     <item>Int32</item>
+        ///     <item>Boolean</item>
+        ///     <item>Byte[]</item>
+        /// </list>
+        /// </remarks>
+        void SetConfigEntry(Object data, String entryName, String node);
+        /// <summary>
+        /// Deletes Certification Authority configuration entry.
+        /// </summary>
+        /// <param name="entryName">Configuration value name to delete.</param>
+        /// <param name="node">Configuration node path under Configuration node. Optional.</param>
+        void DeleteConfigEntry(String entryName, String node);
+    }
+}
---
+++ PKI/Dcom/Implementations/CertRegManagerD.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CERTADMINLib;
+using PKI.Utils;
+
+namespace SysadminsLV.PKI.Dcom.Implementations {
+    /// <summary>
+    /// Represents a managed implementation of <see cref="ICertRegManagerD"/> interface and used to manage ADCS Certification Authority configuration.
+    /// </summary>
+    public class CertRegManagerD : ICertRegManagerD {
+        readonly String _configString;
+
+        /// <summary>
+        /// Initializes a new instance of <strong>CertRegManagerD</strong> class from a Certification Authority configuration string.
+        /// </summary>
+        /// <param name="configString"></param>
+        /// <exception cref="ArgumentNullException">
+        /// <strong>configString</strong> parameter is null.
+        /// </exception>
+        public CertRegManagerD(String configString) {
+            _configString = configString ?? throw new ArgumentNullException(nameof(configString));
+        }
+
+        /// <inheritdoc />
+        public Object GetConfigEntry(String entryName, String node = "") {
+            ICertAdmin2 certAdmin = new CCertAdminClass();
+            try {
+                return certAdmin.GetConfigEntry(_configString, node ?? String.Empty, entryName ?? String.Empty);
+            } catch (Exception ex) {
+                if (ex is FileNotFoundException) {
+                    return null;
+                }
+                throw;
+            } finally {
+                CryptographyUtils.ReleaseCom(certAdmin);
+            }
+        }
+        /// <inheritdoc />
+        public T GetConfigEntry<T>(String entryName, String node = "") {
+            ICertAdmin2 certAdmin = new CCertAdminClass();
+            try {
+                return (T)certAdmin.GetConfigEntry(_configString, node ?? String.Empty, entryName ?? String.Empty);
+            } finally {
+                CryptographyUtils.ReleaseCom(certAdmin);
+            }
+        }
+        /// <inheritdoc />
+        public String GetStringConfigEntry(String entryName, String node = "") {
+            return GetConfigEntry<String>(entryName, node);
+        }
+        /// <inheritdoc />
+        public String[] GetMultiStringConfigEntry(String entryName, String node = "") {
+            return GetConfigEntry<String[]>(entryName, node);
+        }
+        /// <inheritdoc />
+        public Int32 GetNumericConfigEntry(String entryName, String node = "") {
+            return GetConfigEntry<Int32>(entryName, node);
+        }
+        /// <inheritdoc />
+        public Boolean GetBooleanConfigEntry(String entryName, String node = "") {
+            return GetConfigEntry<Int32>(entryName, node) != 0;
+        }
+        /// <inheritdoc />
+        public Byte[] GetBinaryConfigEntry(String entryName, String node = "") {
+            return GetConfigEntry<Byte[]>(entryName, node);
+        }
+        /// <inheritdoc />
+        public void SetConfigEntry(Object data, String entryName, String node = "") {
+            if (data == null) {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            ICertAdmin2 certAdmin = new CCertAdminClass();
+
+            try {
+                switch (data) {
+                    case String _:
+                    case Int32 _:
+                        certAdmin.SetConfigEntry(_configString, node, entryName, data);
+                        break;
+                    case Boolean b:
+                        certAdmin.SetConfigEntry(_configString, node, entryName, b ? 1 : 0);
+                        break;
+                    case IEnumerable<String> array:
+                        certAdmin.SetConfigEntry(_configString, node, entryName, array.ToArray());
+                        break;
+                    case IEnumerable<Byte> array:
+                        certAdmin.SetConfigEntry(_configString, node, entryName, array.ToArray());
+                        break;
+                    default:
+                        throw new ArgumentException();
+
+                }
+            } finally {
+                CryptographyUtils.ReleaseCom(certAdmin);
+            }
+        }
+        /// <inheritdoc />
+        public void DeleteConfigEntry(String entryName, String node = "") {
+            ICertAdmin2 certAdmin = new CCertAdminClass();
+            try {
+                certAdmin.SetConfigEntry(_configString, node ?? String.Empty, entryName ?? String.Empty, null);
+            } catch (Exception ex) {
+                if (!(ex is FileNotFoundException)) {
+                    throw;
+                }
+            } finally {
+                CryptographyUtils.ReleaseCom(certAdmin);
+            }
+        }
+    }
+}
