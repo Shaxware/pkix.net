239c3530f807923b258da136e514e3f3ce7ea4f5
Vadims Podans <vpodans@sysadmins.lv>
2020-06-15 18:28:20 +0300

    added implementation for ICertDbAdminD

--- PKI/Dcom/ICertDbAdminD.cs
+++ PKI/Dcom/ICertDbAdminD.cs
@@ -12,12 +12,12 @@ namespace SysadminsLV.PKI.Dcom {
         /// </summary>
         /// <param name="requestID">RequestID from Certification Authority database.</param>
         /// <returns>Status of approval operation.</returns>
-        AdcsPropCertState ApproveRequest(UInt32 requestID);
+        AdcsPropCertState ApproveRequest(Int32 requestID);
         /// <summary>
         /// Denies specified pending certificate request.
         /// </summary>
         /// <param name="requestID">RequestID from Certification Authority database.</param>
-        void DenyRequest(UInt32 requestID);
+        void DenyRequest(Int32 requestID);
         /// <summary>
         /// Revokes issued or revoked certificate by its serial number.
         /// </summary>
@@ -31,37 +31,40 @@ namespace SysadminsLV.PKI.Dcom {
         /// <param name="requestID">RequestID or RowID from Certification Authority database.</param>
         /// <param name="table">Specifies the target database table.</param>
         /// <returns>Number of removed rows. Can be 0 or 1.</returns>
-        UInt32 DeleteDatabaseRow(UInt32 requestID, AdcsDbCRTable table = AdcsDbCRTable.Request);
+        Int32 DeleteDatabaseRow(Int32 requestID, AdcsDbCRTable table = AdcsDbCRTable.Request);
         /// <summary>
         /// Performs database cleanup by removing expired certificate requests. All certificate rows that expired before specified date and time
         /// are deleted.
         /// </summary>
         /// <param name="notAfter">The date and time of certificate expiration.</param>
         /// <returns>Number of removed rows.</returns>
-        UInt32 DeleteExpiredRequests(DateTime notAfter);
+        Int32 DeleteExpiredRequests(DateTime notAfter);
         /// <summary>
         /// Performs database cleanup by removing expired CRLs. All CLR rows that expired before specified date and time
         /// are deleted.
         /// </summary>
         /// <param name="notAfter">The date and time of CRL expiration.</param>
         /// <returns>Number of removed rows.</returns>
-        UInt32 DeleteExpiredCRLs(DateTime notAfter);
+        Int32 DeleteExpiredCRLs(DateTime notAfter);
         /// <summary>
         /// Performs database cleanup by removing requests that were last updated before specified date and time.
         /// </summary>
         /// <param name="notAfter">The date and time of last request row update.</param>
         /// <returns>Number of removed rows.</returns>
-        UInt32 DeleteLastUpdatedRequests(DateTime notAfter);
+        Int32 DeleteLastUpdatedRequests(DateTime notAfter);
         /// <summary>
         /// Imports certificate to Certification Authority database.
         /// </summary>
         /// <param name="certificate">A certificate to import.</param>
+        /// <exception cref="ArgumentNullException"><strong>certificate</strong> parameter is null.</exception>
         /// <returns>RequestID from request table.</returns>
-        UInt32 ImportCertificate(X509Certificate2 certificate);
+        Int32 ImportCertificate(X509Certificate2 certificate);
         /// <summary>
         /// Gets the status of certificate by its serial number.
         /// </summary>
         /// <param name="serialNumber">A hexadecimal string that represents a serial number.</param>
+        /// <exception cref="ArgumentNullException"><strong>serialNumber</strong> parameter is null.</exception>
+        /// <exception cref="ArgumentException"><strong>serialNumber</strong> parameter is not valid serial number string.</exception>
         /// <returns>Status of certificate.</returns>
         AdcsPropCertState GetCertificateStatus(String serialNumber);
     }
---
+++ PKI/Dcom/Implementations/ImportForeignOption.cs
@@ -0,0 +1,6 @@
+﻿namespace SysadminsLV.PKI.Dcom.Implementations {
+    enum ImportForeignOption {
+        AllowForeign   = 0x10000,
+        UpdateExisting = 0x20000
+    }
+}
