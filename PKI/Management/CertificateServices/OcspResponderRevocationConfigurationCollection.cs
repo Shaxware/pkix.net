d34699bb99118313f6e802fd2d0386dd08fccbd0
Vadims Podans <vpodans@sysadmins.lv>
2020-06-18 09:08:27 +0300

    added indexer by configuration name

--- PKI/Management/CertificateServices/OcspResponderRevocationConfigurationCollection.cs
+++ PKI/Management/CertificateServices/OcspResponderRevocationConfigurationCollection.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SysadminsLV.PKI.Management.CertificateServices {
     /// <summary>
@@ -8,8 +10,8 @@ namespace SysadminsLV.PKI.Management.CertificateServices {
         /// <inheritdoc />
         public OcspResponderRevocationConfigurationCollection() { }
         /// <inheritdoc />
-        public OcspResponderRevocationConfigurationCollection(IEnumerable<OcspResponderRevocationConfiguration> collection) : base(collection) {
-            
-        }
+        public OcspResponderRevocationConfigurationCollection(IEnumerable<OcspResponderRevocationConfiguration> collection) : base(collection) { }
+
+        public OcspResponderRevocationConfiguration this[String name] => InternalList.FirstOrDefault(x => x.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
     }
 }
