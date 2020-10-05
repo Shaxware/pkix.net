34410729e4b21907cd8ba0ce1eeeb1adc7680032
Vadims Podāns <vpodans@sysadmins.lv>
2020-10-05 17:14:15 +0300

    removed tabs in the middle

--- PKI/CertificateTemplates/CertificateTemplateNameFlags.cs
+++ PKI/CertificateTemplates/CertificateTemplateNameFlags.cs
@@ -10,64 +10,64 @@ namespace PKI.CertificateTemplates {
         /// <summary>
         /// This flag instructs the client to supply subject information in the certificate request
         /// </summary>
-        EnrolleeSuppliesSubject				= 0x00000001, // 1
+        EnrolleeSuppliesSubject          = 0x00000001, // 1
         /// <summary>
         /// This flag instructs the client to reuse values of subject name and alternative subject name extensions from an existing valid
         /// certificate when creating a certificate renewal request.
         /// <para><strong>Windows Server 2003, Windows Server 2008</strong> - this flag is not supported.</para>
         /// </summary>
-        OldCertSuppliesSubjectAndAltName	= 0x00000008, // 8
+        OldCertSuppliesSubjectAndAltName = 0x00000008, // 8
         /// <summary>
         /// This flag instructs the client to supply subject alternate name information in the certificate request.
         /// </summary>
-        EnrolleeSuppluiesAltSubject			= 0x00010000, // 65536
+        EnrolleeSuppluiesAltSubject      = 0x00010000, // 65536
         /// <summary>
         /// This flag instructs the CA to add the value of the requester's FQDN and NetBIOS name to the Subject Alternative Name extension
         /// of the issued certificate.
         /// </summary>
-        AltSubjectRequireDomainDNS			= 0x00400000, // 4194304
+        AltSubjectRequireDomainDNS       = 0x00400000, // 4194304
         /// <summary>
         /// This flag instructs the CA to add the value of the requester's Service Principal name to the Subject Alternative Name extension
         /// of the issued certificate.
         /// </summary>
-        AltSubjectRequireSPN = 0x00800000, // 8388608
+        AltSubjectRequireSPN             = 0x00800000, // 8388608
         /// <summary>
         /// This flag instructs the CA to add the value of the objectGUID attribute from the requestor's user object in Active Directory to the
         /// Subject Alternative Name extension of the issued certificate.
         /// </summary>
-        AltSubjectRequireDirectoryGUID		= 0x01000000, // 16777216
+        AltSubjectRequireDirectoryGUID   = 0x01000000, // 16777216
         /// <summary>
         /// This flag instructs the CA to add the value of the UPN attribute from the requestor's user object in Active Directory to the
         /// Subject Alternative Name extension of the issued certificate.
         /// </summary>
-        AltSubjectRequireUPN				= 0x02000000, // 33554432
+        AltSubjectRequireUPN             = 0x02000000, // 33554432
         /// <summary>
         /// This flag instructs the CA to add the value of the e-mail attribute from the requestor's user object in Active Directory to the
         /// Subject Alternative Name extension of the issued certificate.
         /// </summary>
-        AltSubjectRequireEmail				= 0x04000000, // 67108864
+        AltSubjectRequireEmail           = 0x04000000, // 67108864
         /// <summary>
         /// This flag instructs the CA to add the value obtained from the DNS attribute of the requestor's user object in Active Directory to the
         /// Subject Alternative Name extension of the issued certificate.
         /// </summary>
-        AltSubjectRequireDNS				= 0x08000000, // 134217728
+        AltSubjectRequireDNS             = 0x08000000, // 134217728
         /// <summary>
         /// This flag instructs the CA to add the value obtained from the DNS attribute of the requestor's user object in Active Directory as the
         /// CN in the subject of the issued certificate.
         /// </summary>
-        SubjectRequireDNSasCN				= 0x10000000, // 268435456
+        SubjectRequireDNSasCN            = 0x10000000, // 268435456
         /// <summary>
         /// This flag instructs the CA to add the value of the e-mail attribute from the requestor's user object in Active Directory as the
         /// subject of the issued certificate.
         /// </summary>
-        SubjectRequireEmail					= 0x20000000, // 536870912
+        SubjectRequireEmail              = 0x20000000, // 536870912
         /// <summary>
         /// This flag instructs the CA to set the subject name to the requestor's CN from Active Directory.
         /// </summary>
-        SubjectRequireCommonName			= 0x40000000, // 1073741824
+        SubjectRequireCommonName         = 0x40000000, // 1073741824
         /// <summary>
         /// This flag instructs the CA to set the subject name to the requestor's distinguished name (DN) from Active Directory.
         /// </summary>
-        SubjectrequireDirectoryPath			= unchecked((Int32)0x80000000)  // -2147483648
+        SubjectrequireDirectoryPath      = unchecked((Int32)0x80000000)  // -2147483648
     }
 }
